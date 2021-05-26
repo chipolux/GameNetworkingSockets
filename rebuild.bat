bf59fece57027ef0e7d2817570774b9ae3940cee
NaKyle <nakyle.wright@gmail.com>
2021-05-26 11:39:46 -0500

    Add script for rebuilding cause I'm too lazy to type...

---
+++ rebuild.bat
@@ -0,0 +1,7 @@
+IF NOT EXIST .\vcpkg\ (
+    git clone https://github.com/microsoft/vcpkg
+    .\vcpkg\bootstrap-vcpkg.bat
+)
+.\vcpkg\vcpkg --overlay-ports=vcpkg_ports remove gamenetworkingsockets --triplet=x64-windows
+rmdir /s /q  %LOCALAPPDATA%\vcpkg\archives
+.\vcpkg\vcpkg --overlay-ports=vcpkg_ports install gamenetworkingsockets --triplet=x64-windows
