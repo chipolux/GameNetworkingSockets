3f9203227d5cb18979e6f64d62d59605d4f2c958
NaKyle <nakyle.wright@gmail.com>
2021-05-26 23:46:00 -0500

    Remove build directories as well.

--- rebuild-win64.bat
+++ rebuild-win64.bat
@@ -1,7 +1,9 @@
+@echo off
 IF NOT EXIST .\vcpkg\ (
     git clone https://github.com/microsoft/vcpkg
     .\vcpkg\bootstrap-vcpkg.bat
 )
 .\vcpkg\vcpkg --overlay-ports=vcpkg_ports remove gamenetworkingsockets --triplet=x64-windows
 rmdir /s /q  %LOCALAPPDATA%\vcpkg\archives
+rmdir /s /q  .\vcpkg\buildtrees
 .\vcpkg\vcpkg --overlay-ports=vcpkg_ports install gamenetworkingsockets --triplet=x64-windows
